4826aefa35b4bd71bc92481993584805d22304f1
Joyce Benson <bensonjoyce25@gmail.com>
2024-12-08 19:09:30 +0300

    Pass the tests

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';  // Define your currentUser here
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;  // Concatenate the greeting with currentUser
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();  // Convert the welcome message to uppercase
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;  // Get the first initial of currentUser
+
